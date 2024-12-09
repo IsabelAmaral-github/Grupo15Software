9456180044852e7020e76c031fa4072c88546d38
IsabelAmaral-github <isabelpucrio@gmail.com>
2024-12-09 18:02:27 -0300

    carrinho feito

---
+++ hello_world/templates/js/login.js
@@ -0,0 +1,8 @@
+    // Seleciona o botão pelo ID
+    const buttonIndex = document.getElementById('buttonIndexL');
+
+    // Adiciona um evento de clique ao botão
+    buttonIndex.addEventListener('click', function () {
+        // Redireciona para o index.html
+        window.location.href = 'index.html';
+    });
